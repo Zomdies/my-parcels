837cc271da312be7db437eeb61156d0976e2c30c
Zomdies <sablezubi@mail.ru>
2019-12-18 19:25:35 +0700

    First Commit

---
+++ src/panels/Home.js
@@ -0,0 +1,209 @@
+import React, {  useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import Panel from '@vkontakte/vkui/dist/components/Panel/Panel';
+import PanelHeader from '@vkontakte/vkui/dist/components/PanelHeader/PanelHeader';
+
+import ModalPage from '@vkontakte/vkui/dist/components/ModalPage/ModalPage'
+import ModalRoot from '@vkontakte/vkui/dist/components/ModalRoot/ModalRoot'
+import ModalPageHeader from '@vkontakte/vkui/dist/components/ModalPageHeader/ModalPageHeader'
+import HeaderButton from '@vkontakte/vkui/dist/components/HeaderButton/HeaderButton'
+import FormLayout from '@vkontakte/vkui/dist/components/FormLayout/FormLayout'
+import Button from '@vkontakte/vkui/dist/components/Button/Button';
+import Group from '@vkontakte/vkui/dist/components/Group/Group';
+import List from '@vkontakte/vkui/dist/components/List/List'
+import Cell from '@vkontakte/vkui/dist/components/Cell/Cell';
+import InfoRow from '@vkontakte/vkui/dist/components/InfoRow/InfoRow'
+import Div from '@vkontakte/vkui/dist/components/Div/Div';
+import Avatar from '@vkontakte/vkui/dist/components/Avatar/Avatar';
+
+import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
+import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
+import Icon24Done from '@vkontakte/icons/dist/24/done';
+
+import icon from '../img/icon.png'
+import icon_add from '../img/add_icon.png'
+
+import '../css/Home.css'
+import { render } from 'react-dom';
+
+function sendRequest(method, url){
+	return new Promise( (resolve, reject) =>{
+		const xhr = new XMLHttpRequest();
+		xhr.open(method,url);
+		xhr.responseType = 'json';
+		xhr.onload = () => {
+			if (xhr.status >= 400) {
+				reject(xhr.response);
+			}else{
+				resolve(xhr.response);
+			}
+		}
+		xhr.onerror = () => {
+			reject(xhr.response);
+		}
+		xhr.send();
+	});
+}
+
+function load_info(setPopout,chooseParcels,setParcelsData){
+
+    
+    var  carriers = "";
+    
+    fetch("https://cors-anywhere.herokuapp.com/https://moyaposylka.ru/api/v1/trackers/"+chooseParcels[1]+"/"+chooseParcels[2])
+    .then(response => response.json())
+    .then(result =>{
+        try{
+            if (result.status) {setParcelsData(null);
+                console.log(result)}
+                else{
+                    console.log(result)
+                    setParcelsData(result);
+                }
+        }catch{
+        }
+        setPopout(null);
+
+    }).catch(err => console.log(err))
+
+}
+
+const Home = props => {
+	const id_v = new URLSearchParams(window.location.href).get("vk_user_id");
+	const [activeModal, setActiveModal] = useState(null);
+	const [serverData, setServerData] = useState(null);
+	const [parcelsData, setParcelsData] = useState(null);
+	const [chooseParcels, setChooseParcels] = useState(null);
+	var Barcode = require('react-barcode');
+	// const parcels = ["Мая посылка 1","Мая посылка 2","Мая посылка 3"];
+	const [parcels , setParcels] = useState(["Мая посылка 1","Мая посылка 2","Мая посылка 3"]); //
+
+	useEffect(() => {
+		props.setPopout(<ScreenSpinner size='large' />);
+		fetch("https://vk-hack.herokuapp.com/get/id"+id_v)
+				.then(response => response.json())
+    			.then(result =>{
+        			try{
+            			if (result.status) {
+							setServerData(null);
+                			console.log(result)}
+                		else{
+							var a = Object.values(result.tracks["track1"]);
+                    		// console.log(a)
+							setServerData(result)
+							setChooseParcels(a);
+							props.setPopout(null);
+               			}
+        			}catch{
+					}
+					props.setPopout(null);
+
+    	}).catch(err => console.log(err))
+		
+		// async function fetchData2() {
+		
+		// 		const server = await sendRequest("GET","https://vk-hack.herokuapp.com/get/id"+id_v); //164078040
+		// 		setServerData(server);
+		// 		setChooseParcels(server.tracks["track1"]);
+		// 		props.setPopout(null);
+		// 		// setPopout(<ScreenSpinner size='large' />);
+			
+		// }
+
+		// fetchData2();
+		// fetchData3();
+		
+		
+	}, []);
+
+	var open_information = (name) =>{
+		setChooseParcels(name);
+		if (chooseParcels != "None") {
+			// console.log(chooseParcels);
+			props.setPopout(<ScreenSpinner size='large' />);
+			load_info(props.setPopout,chooseParcels,setParcelsData);
+			setActiveModal("modal_inform");
+		}
+		
+	}
+	const closeModal = () =>{
+		setActiveModal(null);
+		props.setActivePanel("home")
+	} 
+		
+	
+	return (
+		
+		<Panel id={props.id} theme="white">
+			<PanelHeader>Мои посылки</PanelHeader>
+            <Group title="Посылки">
+				{serverData &&                 	
+					<List>
+                		{serverData && serverData.tracks && Object.values(serverData.tracks).map(v => Object.values(v))
+					.map((item,index) =>{ 
+							return(
+                    			<Cell className="ParcelsName"  id = {index} key={item}  draggable onDragFinish={({ from, to }) => {
+                      					// const parcels1 = [...parcels];
+                      					// parcels1.splice(from, 1);
+                      					// parcels1.splice(to, 0, parcels[from]);
+										  // setParcels(parcels1);
+										//   console.log(item);
+								}}  > <div onClick={() =>{ props.setPopout(null);
+														   open_information(item)} } >{item[0]}</div></Cell>
+							  )})
+							  
+						}	 
+                	</List>
+				}
+				{serverData && Object.keys(serverData.tracks).length == 0 &&
+					<Cell className="ErrorParcels" >Пока у вас нет пасылок</Cell> 
+				}
+				<Cell className="AddParcels" onClick={() => open_information("Добавить посылку")}>Добавить посылку</Cell>
+            </Group>
+			<ModalRoot activeModal={activeModal}>
+				
+				<ModalPage id ="modal_inform"
+				onClose={closeModal}
+				header={
+					<ModalPageHeader className="ModalHaeder"
+						left={<HeaderButton onClick={closeModal}><Icon24Cancel /></HeaderButton>}
+              			right={<HeaderButton onClick={closeModal}><Icon24Done /></HeaderButton>}
+					>Information</ModalPageHeader>
+				}>
+					<FormLayout className="ModalBody">
+						{chooseParcels && 
+							<div>
+								<p>Название посылки : {chooseParcels[0]}</p>
+								<p>Номер отслеживания : {chooseParcels[2]}</p>
+								{parcelsData &&
+									<div className="ModalDopInfo">
+										<p className="ModalDopInfoTime">Время в пути : {parcelsData.deliveringTime} дней    </p>
+										<p className="ModalDopInfoWeight">Вес посылки : {parcelsData.weight} кг.</p>
+									</div>
+								}
+								
+								<Barcode value={chooseParcels[2]} background="white"></Barcode>
+								{parcelsData &&
+									<InfoRow>Статус отслеживания : {parcelsData.events[0].operation}</InfoRow>
+								}
+							
+							</div>
+							
+							
+						}
+						<Button size="xl" onClick={() => console.log("DELETE")}>Больше информации</Button>
+						<Button size="xl" onClick={() => console.log("DELETE")}>Настройки</Button>
+					</FormLayout>
+				</ModalPage>
+				
+			</ModalRoot>
+		</Panel>
+	);
+	
+}
+
+Home.propTypes = {
+	
+};
+
+export default Home;
